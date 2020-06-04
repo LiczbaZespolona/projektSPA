f2f18e4de90581303d2ef493774e224f2590806a
LiczbaZespolona <liczbazespolona@gmail.com>
2020-06-04 14:47:28 +0200

    Exercise template done..

--- src/it-spa.js
+++ src/it-spa.js
@@ -1,21 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/css/font-awesome.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 //import './it-spa.css';
-import './sass/it-spa.scss';
-import $ from 'jquery';  // Import referencji "$" z pakietu jquery
-import { Router } from './router/router';
-import { nav } from './navigation/nav';
-import { itSpaCart } from './cart/it-spa-cart';
+import "./sass/it-spa.scss";
+import $ from "jquery"; // Import referencji "$" z pakietu jquery
+import { Router } from "./router/router";
+import { nav } from "./navigation/nav";
+import { itSpaCart } from "./cart/it-spa-cart";
+import { exercise } from "./exercise/exercise";
 
-const main = $('main'); //Outlet na zmieniającą się treść
+const main = $("main"); //Outlet na zmieniającą się treść
 
 const router = new Router();
 
-router.mount(main);     //Podpinanie maina by w obiekcie można go było zmieniać
+router.mount(main); //Podpinanie maina by w obiekcie można go było zmieniać
 
 router.init();
 
 main.before(nav()); //podpięcie manu navigacji na początek strony przed outlet
 
-main.after(itSpaCart());    //podpięcie koszyka
-
+main.after(exercise());
+main.after(itSpaCart()); //podpięcie koszyka
--- src/navigation/nav-item.js
+++ src/navigation/nav-item.js
@@ -1,14 +1,12 @@
-import $ from 'jquery';
+import $ from "jquery";
 
 export const navItem = (text, click) => {
-    const navItem = $('<li class="nav-item"></li>');
-    const anchor = $('<a class="btn btn-link text-light"></a>');
-    anchor.text(text);
-    anchor.on('click', click);
-    //console.log(click)
-    navItem.append(anchor); //podpięcie <a> pod <li>
+  const navItem = $('<li class="nav-item mr-2"></li>');
+  const anchor = $('<a class="btn btn-link btn-lg"></a>');
+  anchor.text(text);
+  anchor.on("click", click);
+  //console.log(click)
+  navItem.append(anchor); //podpięcie <a> pod <li>
 
-    return navItem;
-}
-
-
+  return navItem;
+};
