627876e651445a6e1627e45927bfb8d087e1bf3b
LiczbaZespolona <liczbazespolona@gmail.com>
2020-06-04 11:52:55 +0200

    Home template done..

--- src/navigation/nav.js
+++ src/navigation/nav.js
@@ -2,14 +2,20 @@ import $ from "jquery";
 import { navItem } from "./nav-item";
 import { routeChange } from "../router/route-change";
 import { routes } from "../router/routes";
+import logoImage from "./../../image/RachMistrzLogo.png";
 
 export const nav = () => {
-  const navbar = $(`
-    <nav class="navbar navbar-expand navbar-dark bg-dark">
-        <span class="navbar-brand">IT-SPA</span>
-        <ul class="navbar-nav mr-auto"></ul>
-    </nav>
-    `);
+  const navbar = $(`<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top"></nav>`);
+  const anchor = $('<a class="navbar-brand"></a>');
+  anchor.html(`<img src="${logoImage}" alt="Rachmistrz logo." class="mr-1 mt-1" style="width:136px; cursor: pointer;">`);
+  anchor.on("click", () => navbar.trigger(routeChange, { path: "/" }));
+  navbar.append(anchor);
+  const button = $('<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavbar"><span class="navbar-toggler-icon"></span></button>');
+  navbar.append(button);
+  const unorderedList = $(`<ul class="navbar-nav mr-auto"></ul>`);
+  const collapsibleDiv = $('<div class="collapse navbar-collapse" id="mainNavbar"></div>');
+  collapsibleDiv.append(unorderedList);
+  navbar.append(collapsibleDiv);
 
   //const bookingNavItem = navItem("Booking", () => navbar.trigger(routeChange, {path: '/booking'}));   //Emitowanie zdarzenia przez element po kliknięciu
   // mapowanie kolejnych elementów routes na pozycje menu
--- src/views/home.js
+++ src/views/home.js
@@ -1,16 +1,15 @@
-import $ from 'jquery';
+import $ from "jquery";
+import { routeChange } from "../router/route-change";
 
 export const home = () => {
-    const fragment = $(new DocumentFragment());
+  const fragment = $("<section></section>");
 
-    fragment
-        .append('<h2>Home</h2>')
-        .append('<p>Lorem ipsum dolor sit amte...</p>');
+  fragment.append("<h2>Rachmistrz wita!!</h2>").append("<p>Ćwicz z nami tabliczkę mnozenia.</p>").append("<p>Zmierz się z innymi i pokaż co potrafisz!</p>");
 
+  const anchor = $('<a class="btn btn-lg btn-success text-light"></a>');
+  anchor.text("Zacznij juz teraz!");
+  anchor.on("click", () => fragment.trigger(routeChange, { path: "/exercise" }));
+  fragment.append(anchor);
 
-
-
-
-
-    return Promise.resolve(fragment);
-}
+  return Promise.resolve(fragment);
+};
